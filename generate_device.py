31a8605c33af6f15250bdf67145908ef07f8bc25
kirtipahwa <pahwakirit09942@gmail.com>
2024-05-15 12:00:17 +0530

    First commit

---
+++ generate_device.py
@@ -0,0 +1,72 @@
+import subprocess
+import json
+import requests
+import boto3
+
+def generate_device(count_of_devices):
+    for i in range(1, count_of_devices + 1):
+            try:
+                command = ["openssl", "genrsa", "-out",  "deviceCert_{}.key".format(i), "2048"]
+                subprocess.run(command, check=True)
+                print("RSA private key generated successfully for device")
+
+
+                req_command = ["openssl", "req", "-new", "-key",  "deviceCert_{}.key".format(i), "-out","deviceCert_{}.csr".format(i)]
+                input_values = "US\n\n\nTulip\nTulip-dev\nTulip-thing-{}\n\n\n\n".format(i)
+                subprocess.run(req_command, input=input_values.encode(), check=True)
+                print("Certificate Signing Request (CSR) generated successfully for device")
+
+                crt_command = ["openssl", "x509", "-req", "-in","deviceCert_{}.csr".format(i), "-CA", "deviceRootCA.pem", "-CAkey",  "deviceRootCA.key" , "-CAcreateserial", "-out", "deviceCert_{}.crt".format(i), "-days", "365", "-sha256"]
+                subprocess.run(crt_command, check=True)
+                print("Certificate (CRT) generated successfully for device")
+
+                with open("deviceCert_{}.crt".format(i), "rb") as device_cert_file:
+                    with open("deviceRootCA.pem", "rb") as ca_cert_file:
+                        with open("deviceCertAndCACert_{}.crt".format(i), "wb") as output_file:
+                            output_file.write(device_cert_file.read())
+                            output_file.write(ca_cert_file.read())
+                print("Certificate concatenated successfully for device")
+
+                mosquitto_command_specific = [
+                "mosquitto_pub",
+                    "-h", "a163sgg7lpdtqv-ats.iot.us-east-1.amazonaws.com",
+                    "--cafile", "awsRootCA.pem",
+                    "--cert", "deviceCertAndCACert_{}.crt".format(i),  
+                    "--key", "deviceCert_{}.key".format(i),
+                    "-p", "8883",
+                    "-q", "1",
+                    "-t", "Tulip/Topic",
+                    "-i", "Tulip-Thing-{}".format(i),
+                    "--tls-version", "tlsv1.2",
+                    "-m", "Hello",
+                    "-d"
+                ]
+                subprocess.run(mosquitto_command_specific, check=True)
+                print("Message published successfully for device")
+                
+                
+
+            except subprocess.CalledProcessError as e:
+                print("Error:", e)
+            
+            list_command = ["aws", "iot", "list-thing-principals", "--thing-name", "Tulip-thing-{}".format(i)]
+            result = subprocess.run(list_command, capture_output=True,  check=True)
+
+                # Parse JSON output
+            data = json.loads(result.stdout)
+
+                # Extract certificate ARN
+            certificate_arn = data["principals"][0].split("/")[-1]
+
+                # Attach policy to certificate
+            attach_command = [
+                        "aws", "iot", "attach-policy",
+                        "--policy-name", "tulip-alias-policy",
+                        "--target", "arn:aws:iot:us-east-1:705158173663:cert/{}".format(certificate_arn)
+                    ]
+            subprocess.run(attach_command, check=True)
+
+            print("Policy attached successfully to certificate:", certificate_arn)
+
+
+
---
+++ s3_access.py
@@ -0,0 +1,55 @@
+import boto3
+import requests
+from generate_token import generate_token
+import os
+
+# Specify the bucket name and object key
+bucket_name = 'my-simulator-k-bucket'
+object_key = 'File1'
+path="./docs.txt"
+
+def s3_access(device_number):
+    try:
+        access_credentials=generate_token(device_number)
+        if access_credentials is None:
+                print("Failed to generate token")
+
+        else:
+                access_key_id = access_credentials.get('accessKeyId')
+                secret_access_key = access_credentials.get('secretAccessKey')
+                session_token = access_credentials.get('sessionToken')
+                # print("Access Key ID:", access_key_id)
+                # print("Secret Access Key:", secret_access_key)
+                # print("Session Token:", session_token)
+                
+                s3 = boto3.client('s3',aws_access_key_id=access_key_id,aws_secret_access_key=secret_access_key,aws_session_token=session_token)
+                # s3 = boto3.client('s3')
+                print("Enter 1: If you want to upload data into the s3 bucket")
+                print("Enter 2: If you want to read data from the s3 bucket")
+                n=int(input())
+                if n==1:
+                    # fetch data from the s3 bucket
+                    try:
+                        s3.upload_file(path, bucket_name, object_key)
+                        print("Upload successful!")
+                    except Exception as e:
+                        print("Error uploading to S3:", e)
+                elif n==2:
+                        try:
+                            print(bucket_name)
+                            response = s3.get_object(Bucket=bucket_name, Key=object_key)
+                            data = response['Body'].read()
+                            print("Data from S3:", data)
+                        except Exception as e:
+                            print("Error reading from S3:", e)
+
+    except requests.exceptions.RequestException as e:
+        # Request encountered an exception, print the error
+        print("Error:", e)
+
+            
+
+
+       
+
+
